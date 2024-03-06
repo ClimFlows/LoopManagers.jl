b108573cd2315d60f95d3c2292d338646f821355
Thomas Dubos <thomas.dubos@lmd.ipsl.fr>
2024-03-07 00:30:49 +0100

    Add some tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,38 @@
 using LoopManagers
+using ManagedLoops: @loops, @vec
 using Test
+
+@loops function loop!(_, fun, a, b)
+    let (irange, jrange) = axes(a)
+        @inbounds for j in jrange
+            for i in irange
+                a[i,j] = fun(b[i,j])
+            end
+        end
+    end
+end
+
+function test(mgr, fun, b)
+    a = similar(b)
+    @info mgr
+    loop!(mgr, fun, a, b)
+    @time loop!(mgr, fun, a, b)
+    return b
+end
+
+managers = Any[nothing, LoopManagers.PlainCPU(),
+    LoopManagers.VectorizedCPU(),
+    LoopManagers.MultiThread(),
+    LoopManagers.MultiThread(VectorizedCPU()),
+    LoopManagers.PlainCPU()]
 
 @testset "LoopManagers.jl" begin
     # Write your tests here.
+    let fun = exp, b=randn(100,100)
+        for mgr in managers
+            test(mgr, fun, b)
+            @test true
+        end
+    end
+
 end
