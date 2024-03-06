21016f94ceaa37cd696fbac6937facfe86600245
Documenter.jl <documenter@juliadocs.github.io>
2024-03-06 22:14:54 +0000

    build based on 9878d2c

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = LoopManagers","category":"page"},{"location":"#LoopManagers","page":"Home","title":"LoopManagers","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for LoopManagers.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [LoopManagers]","category":"page"}]
+}
