d0dab939e23799b9e6a9a84ed6ff83068242e2fa
Thomas Dubos <thomas.dubos@lmd.ipsl.fr>
2024-03-07 18:15:09 +0100

    Test with SIMDMathFunctions

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,31 +1,26 @@
 using LoopManagers
 using ManagedLoops: @loops, @vec
-using InteractiveUtils: versioninfo
+using SIMDMathFunctions
+
+using InteractiveUtils: versioninfo, @code_native
+using Chairmarks: @b, @be
 using Test
 
 versioninfo()
 
-# forces specialization on function argument
-struct Functor{Fun}
-    fun::Fun
-end
-@inline (fun::Functor)(args...) = fun.fun(args...)
-
 @loops function loop!(_, fun, a, b)
     let (irange, jrange) = axes(a)
-        @inbounds @vec for i in irange, j in jrange
-            a[i, j] = fun(b[i, j])
+        @vec for i in irange, j in jrange
+            @inbounds a[i, j] = fun(b[i, j])
         end
     end
 end
 
 function test(mgr, b)
     a = similar(b)
-    fun = Functor(exp)
     @info mgr
-    loop!(mgr, fun, a, b)
-    @time loop!(mgr, fun, a, b)
-    return b
+    display(@be loop!(mgr, exp, a, b))
+    return nothing
 end
 
 managers = Any[
@@ -38,7 +33,7 @@ managers = Any[
 
 @testset "LoopManagers.jl" begin
     # Write your tests here.
-    let b = randn(1000, 1000)
+    let b = randn(1024, 1024)
         for mgr in managers
             test(mgr, b)
             @test true
