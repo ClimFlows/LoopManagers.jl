5dd75a66eee2e3e851106978dbe4c6fcb7371f98
Thomas Dubos <prenom.nom@lmd.ipsl.fr>
2024-04-12 21:23:30 +0200

    improve GPU perf

--- ext/KA_Ext.jl
+++ ext/KA_Ext.jl
@@ -12,8 +12,8 @@ LoopManagers.KernelAbstractions_GPU(gpu::G, A) where G = KA_GPU{A,G}(gpu)
 
 @inline function offload(fun, backend::KA_GPU, irange::Range1, args...)
     (; gpu) = backend
-    kernel = kernel_KA_1D(gpu, 32)
-    kernel(fun, first(irange)-1, args... ; ndrange=length(irange))
+    kernel = kernel_KA_1D(gpu)
+    kernel(fun, first(irange)-1, args ; ndrange=length(irange))
     KernelAbstractions.synchronize(gpu)
     return nothing
 end
@@ -23,17 +23,17 @@ end
     M, N = length(irange), length(jrange)
     i0, j0 = first(irange)-1, first(jrange)-1
     kernel = kernel_KA_2D(gpu, (32,32), (32,N))
-    kernel(fun, i0, j0, last(irange), args... )
+    kernel(fun, i0, j0, last(irange), args)
     KernelAbstractions.synchronize(gpu)
     return nothing
 end
 
-@kernel function kernel_KA_1D(fun, i0, args...)
+@kernel function kernel_KA_1D(fun, i0, args)
     i = @index(Global, Linear)
     @inline fun((i+i0,), args...)
 end
 
-@kernel function kernel_KA_2D(fun, i0, j0, M, args...)
+@kernel function kernel_KA_2D(fun, i0, j0, M, args)
     i, j = @index(Global, NTuple)
     ranges = (i+i0):32:M, (j+j0,)
     @inline fun(ranges, args...)
