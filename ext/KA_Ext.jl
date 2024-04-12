72ef31dcd9ebcd3ce934616465b8ab2accae470c
Thomas Dubos <dubos@lmd.polytechnique.fr>
2024-04-13 01:01:33 +0200

    Support adapt_storage and synchronize for GPU (#7)
    * improve GPU perf
    
    * Implement synchronize
    
    ---------
    
    Co-authored-by: Thomas Dubos <prenom.nom@lmd.ipsl.fr>

--- ext/KA_Ext.jl
+++ ext/KA_Ext.jl
@@ -1,20 +1,23 @@
 module KA_Ext
 
-using KernelAbstractions
+using KernelAbstractions: @kernel, @index, synchronize as KA_sync
+import KernelAbstractions.Adapt.adapt_storage
 
 using LoopManagers: LoopManagers, Range1, Range2
-import ManagedLoops: offload, DeviceManager
+import ManagedLoops: synchronize, offload, DeviceManager
 
 struct KA_GPU{A, G} <: DeviceManager
     gpu::G
 end
 LoopManagers.KernelAbstractions_GPU(gpu::G, A) where G = KA_GPU{A,G}(gpu)
 
+adapt_storage(mgr::KA_GPU, x) = adapt_storage(mgr.gpu, x)
+synchronize(mgr::KA_GPU) = KA_sync(mgr.gpu)
+
 @inline function offload(fun, backend::KA_GPU, irange::Range1, args...)
     (; gpu) = backend
-    kernel = kernel_KA_1D(gpu, 32)
-    kernel(fun, first(irange)-1, args... ; ndrange=length(irange))
-    KernelAbstractions.synchronize(gpu)
+    kernel = kernel_KA_1D(gpu)
+    kernel(fun, first(irange)-1, args ; ndrange=length(irange))
     return nothing
 end
 
@@ -23,17 +26,16 @@ end
     M, N = length(irange), length(jrange)
     i0, j0 = first(irange)-1, first(jrange)-1
     kernel = kernel_KA_2D(gpu, (32,32), (32,N))
-    kernel(fun, i0, j0, last(irange), args... )
-    KernelAbstractions.synchronize(gpu)
+    kernel(fun, i0, j0, last(irange), args)
     return nothing
 end
 
-@kernel function kernel_KA_1D(fun, i0, args...)
+@kernel function kernel_KA_1D(fun, i0, args)
     i = @index(Global, Linear)
     @inline fun((i+i0,), args...)
 end
 
-@kernel function kernel_KA_2D(fun, i0, j0, M, args...)
+@kernel function kernel_KA_2D(fun, i0, j0, M, args)
     i, j = @index(Global, NTuple)
     ranges = (i+i0):32:M, (j+j0,)
     @inline fun(ranges, args...)
