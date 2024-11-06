8ee9e5a51d31b884bf514d335c07c86d97365b60
Thomas Dubos <dubos@llmd.ipsl.fr>
2024-11-06 17:25:08 +0100

    Default to non-blocked iteration

--- ext/KA_Ext.jl
+++ ext/KA_Ext.jl
@@ -1,25 +1,35 @@
 module KA_Ext
 
 using KernelAbstractions: @kernel, @index, synchronize as KA_sync
+using KernelAbstractions.Adapt: adapt, adapt_structure
 import KernelAbstractions.Adapt.adapt_storage
 
 using LoopManagers: LoopManagers, Range1, Range2
 import ManagedLoops: synchronize, offload, DeviceManager
 
-struct KA_GPU{Blocks,G} <: DeviceManager
+struct KA_GPU{Blocks,G,A} <: DeviceManager
     gpu::G
+    adaptor::A # adaptor to device-side arrays
     blocks::Val{Blocks}
 end
-LoopManagers.KernelAbstractions_GPU(gpu, blocks::Tuple{Int,Int} = (0, 0)) =
-    KA_GPU(gpu, Val(blocks))
+LoopManagers.KernelAbstractions_GPU(gpu, adaptor, blocks::Tuple{Int,Int} = (0, 0)) =
+    KA_GPU(gpu, adaptor, Val(blocks))
 
-adapt_storage(mgr::KA_GPU, x) = adapt_storage(mgr.gpu, x)
+adapt_storage(mgr::KA_GPU, x) = adapt_mgr(mgr, x)
+adapt_mgr(_, x) = x
+adapt_mgr(_, x::AbstractUnitRange) = x
+adapt_mgr(mgr, x::AbstractArray) = adapt_storage(mgr.gpu, x)
+
 synchronize(mgr::KA_GPU) = KA_sync(mgr.gpu)
+
+offload(fun, backend::KA_GPU{Blocks}, irange::Range1, args...) where {Blocks} =
+    offload_1D(fun, backend.gpu, Val(Blocks[1]), irange, args)
+offload(fun, backend::KA_GPU{Blocks}, ijrange::Range2, args...) where {Blocks} =
+    offload_2D(fun, backend.gpu, Val(Blocks[2]), ijrange, args)
 
 #========= one-dimensional iteration ==========#
 
-@inline function offload(fun, backend::KA_GPU, irange::Range1, args...)
-    (; gpu) = backend
+function offload_1D(fun::Fun, gpu, ::Val{0}, irange, args) where {Fun}
     kernel = kernel_KA_1D(gpu)
     kernel(fun, first(irange) - 1, args; ndrange = length(irange))
     return nothing
@@ -30,34 +40,25 @@ end
     @inline fun((i + i0,), args...)
 end
 
+function offload_1D(fun::Fun, gpu, ::Val{block}, irange, args) where {Fun,block}
+    kernel = kernel_KA_1D_blocked(gpu, block, length(irange))
+    kernel(fun, first(irange) - 1, Val{block}(), last(irange), args)
+    return nothing
+end
+
+@kernel function kernel_KA_1D_blocked(fun, i0, ::Val{block}, ilast, args) where {block}
+    i = @index(Global, Linear)
+    range = (i+i0):block:ilast
+    @inline fun(range, args...)
+end
+
 #============= two-dimensional iteration =============#
 
-@inline function offload(
-    fun::Fun,
-    backend::KA_GPU{Blocks},
-    (irange, jrange)::Range2,
-    args...,
-) where {Fun,Blocks}
-    (; gpu) = backend
+function offload_2D(fun::Fun, gpu, ::Val{0}, (irange, jrange), args) where {Fun}
     M, N = length(irange), length(jrange)
     i0, j0 = first(irange) - 1, first(jrange) - 1
-    block = Blocks[2] # compile-time constant
-    
-    if block == 0 # no blocking in inner loop
-        kernel = kernel_KA_2D(gpu)
-        kernel(fun, i0, j0, args; ndrange = map(length, (irange, jrange)))
-    else # inner loop is made of blocks of length `block`
-        kernel = kernel_KA_2D_blocked(gpu)
-        kernel(
-            fun,
-            i0,
-            Val(block),
-            last(irange),
-            j0,
-            args;
-            ndrange = (block, length(jrange)),
-        )
-    end
+    kernel = kernel_KA_2D(gpu)
+    kernel(fun, i0, j0, args; ndrange = (M, N))
     return nothing
 end
 
@@ -67,17 +68,18 @@ end
     @inline fun(ranges, args...)
 end
 
-@kernel function kernel_KA_2D_blocked(
-    fun::Fun,
-    i0,
-    ::Val{block},
-    ilast,
-    j0,
-    args,
-) where {Fun,block}
+function offload_2D(fun::Fun, gpu, ::Val{block}, (irange, jrange), args) where {Fun,block}
+    M, N = length(irange), length(jrange)
+    i0, j0 = first(irange) - 1, first(jrange) - 1
+    kernel = kernel_KA_2D_blocked(gpu, block, (block, N))
+    kernel(fun, i0, Val{block}(), last(irange), j0, args; ndrange = (block, N))
+    return nothing
+end
+
+@kernel function kernel_KA_2D_blocked(fun, i0, ::Val{block}, ilast, j0, args) where {block}
     i, j = @index(Global, NTuple)
     ranges = (i+i0):block:ilast, (j + j0,)
     @inline fun(ranges, args...)
 end
 
-end
+end # module
