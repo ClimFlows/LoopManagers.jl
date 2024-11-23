90bc8e23258f40a9b902ef8d2ab8a2956447e041
Thomas Dubos <dubos@lmd.polytechnique.fr>
2024-11-23 16:59:16 +0100

    Add tunable "block" parameter to GPU backend (#16)
    * New API for KA_GPU, accepts tuning parameter `blocks`
    
    * v2.0 (breaks KA_GPU API)
    
    * Default to non-blocked iteration
    
    * Drop unused device adaptor
    
    ---------
    
    Co-authored-by: Thomas Dubos <dubos@llmd.ipsl.fr>

--- ext/KA_Ext.jl
+++ ext/KA_Ext.jl
@@ -1,46 +1,84 @@
 module KA_Ext
 
 using KernelAbstractions: @kernel, @index, synchronize as KA_sync
+using KernelAbstractions.Adapt: adapt, adapt_structure
 import KernelAbstractions.Adapt.adapt_storage
 
 using LoopManagers: LoopManagers, Range1, Range2
 import ManagedLoops: synchronize, offload, DeviceManager
 
-struct KA_GPU{A, G} <: DeviceManager
+struct KA_GPU{Blocks,G} <: DeviceManager
     gpu::G
+    blocks::Val{Blocks}
 end
-LoopManagers.KernelAbstractions_GPU(gpu::G, A) where G = KA_GPU{A,G}(gpu)
+LoopManagers.KernelAbstractions_GPU(gpu, blocks::Tuple{Int,Int} = (0, 0)) =
+    KA_GPU(gpu, Val(blocks))
 
-adapt_storage(mgr::KA_GPU, x) = adapt_storage(mgr.gpu, x)
+adapt_storage(mgr::KA_GPU, x) = adapt_mgr(mgr, x)
+adapt_mgr(_, x) = x
+adapt_mgr(_, x::AbstractUnitRange) = x
+adapt_mgr(mgr, x::AbstractArray) = adapt_storage(mgr.gpu, x)
+
 synchronize(mgr::KA_GPU) = KA_sync(mgr.gpu)
 
-@inline function offload(fun, backend::KA_GPU, irange::Range1, args...)
-    (; gpu) = backend
-    kernel = kernel_KA_1D(gpu)
-    kernel(fun, first(irange)-1, args ; ndrange=length(irange))
-    return nothing
-end
+offload(fun, backend::KA_GPU{Blocks}, irange::Range1, args...) where {Blocks} =
+    offload_1D(fun, backend.gpu, Val(Blocks[1]), irange, args)
+offload(fun, backend::KA_GPU{Blocks}, ijrange::Range2, args...) where {Blocks} =
+    offload_2D(fun, backend.gpu, Val(Blocks[2]), ijrange, args)
 
-@inline function offload(fun::Fun, backend::KA_GPU, (irange, jrange)::Range2, args...) where Fun
-    (; gpu) = backend
-    M, N = length(irange), length(jrange)
-    i0, j0 = first(irange)-1, first(jrange)-1
-#    kernel = kernel_KA_2D(gpu, (32,32), (32,N))
-    kernel = kernel_KA_2D(gpu)
-    kernel(fun, i0, j0, last(irange), args; ndrange=map(length, (irange,jrange)))
+#========= one-dimensional iteration ==========#
+
+function offload_1D(fun::Fun, gpu, ::Val{0}, irange, args) where {Fun}
+    kernel = kernel_KA_1D(gpu)
+    kernel(fun, first(irange) - 1, args; ndrange = length(irange))
     return nothing
 end
 
 @kernel function kernel_KA_1D(fun, i0, args)
     i = @index(Global, Linear)
-    @inline fun((i+i0,), args...)
+    @inline fun((i + i0,), args...)
 end
 
-@kernel function kernel_KA_2D(fun::Fun, i0, j0, M, args) where Fun
+function offload_1D(fun::Fun, gpu, ::Val{block}, irange, args) where {Fun,block}
+    kernel = kernel_KA_1D_blocked(gpu, block, length(irange))
+    kernel(fun, first(irange) - 1, Val{block}(), last(irange), args)
+    return nothing
+end
+
+@kernel function kernel_KA_1D_blocked(fun, i0, ::Val{block}, ilast, args) where {block}
+    i = @index(Global, Linear)
+    range = (i+i0):block:ilast
+    @inline fun(range, args...)
+end
+
+#============= two-dimensional iteration =============#
+
+function offload_2D(fun::Fun, gpu, ::Val{0}, (irange, jrange), args) where {Fun}
+    M, N = length(irange), length(jrange)
+    i0, j0 = first(irange) - 1, first(jrange) - 1
+    kernel = kernel_KA_2D(gpu)
+    kernel(fun, i0, j0, args; ndrange = (M, N))
+    return nothing
+end
+
+@kernel function kernel_KA_2D(fun::Fun, i0, j0, args) where {Fun}
     i, j = @index(Global, NTuple)
-#    ranges = (i+i0):32:M, (j+j0,)
-    ranges = (i+i0,), (j+j0,)
-@inline fun(ranges, args...)
+    ranges = (i + i0,), (j + j0,)
+    @inline fun(ranges, args...)
 end
 
+function offload_2D(fun::Fun, gpu, ::Val{block}, (irange, jrange), args) where {Fun,block}
+    M, N = length(irange), length(jrange)
+    i0, j0 = first(irange) - 1, first(jrange) - 1
+    kernel = kernel_KA_2D_blocked(gpu, block, (block, N))
+    kernel(fun, i0, Val{block}(), last(irange), j0, args; ndrange = (block, N))
+    return nothing
 end
+
+@kernel function kernel_KA_2D_blocked(fun, i0, ::Val{block}, ilast, j0, args) where {block}
+    i, j = @index(Global, NTuple)
+    ranges = (i+i0):block:ilast, (j + j0,)
+    @inline fun(ranges, args...)
+end
+
+end # module
